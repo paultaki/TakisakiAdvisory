0ad49a8e51b022a2526bd348d013ee2b6b9ed0b5
paultaki <paultaki@gmail.com>
2025-04-09 23:04:23 -0700

    w

--- script.js
+++ script.js
@@ -26,11 +26,20 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
       navbar.classList.remove("active");
     }
 
-    document
-      .querySelector(this.getAttribute("href"))
-      .scrollIntoConstrainedView({
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      const headerOffset = 80;
+      const elementPosition = targetElement.getBoundingClientRect().top;
+      const offsetPosition =
+        elementPosition + window.pageYOffset - headerOffset;
+
+      window.scrollTo({
+        top: offsetPosition,
         behavior: "smooth",
       });
+    }
   });
 });
 
