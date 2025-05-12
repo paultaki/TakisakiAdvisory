23a5cef55e16e836fe92c399df8c63cfba2f5c7c
paultaki <paultaki@gmail.com>
2025-05-12 16:37:27 -0700

    d

--- test-email.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Make sure you have the required dependencies
-echo "Installing dependencies..."
-npm install
-
-# Run the test script
-echo "Testing SendGrid API key..."
-node test-sendgrid.js
